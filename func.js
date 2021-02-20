5819f0b251b4870384b5f5e711924123d41ff0cd
AgolaDeveloper <owinoagola00@gmail.com>
2021-02-20 15:03:24 +0300

    first commit

---
+++ func.js
@@ -0,0 +1,11 @@
+// elmt= document.getElementByClassName('main');
+//var el= document.createElement('p');
+//var text= document.createTextNode= 'I LOVE CODING';
+//el.appendChild(text);
+//elmt.appendChild(el);
+
+//var hed=  document.getElementById('he').style.color='blue';
+//hed.style.color= 'solid blue';
+
+var x= document.getElementsByTagName('p')
+x.style.color='red';
